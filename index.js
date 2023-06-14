149a6ecb28461c39f3971b83c1214610387c7316
Roland Amini <aminiroland01@gmail.com>
2023-06-14 05:45:48 -0700

    done js

---
+++ index.js
@@ -0,0 +1,24 @@
+//function Horloge ()
+//{
+   // let temps = new Date();
+//let heurs = temps.getHours();
+//let minites = temps.getMinutes();
+//let secondes = temps.getSeconds();
+
+//if(heurs < 10)
+//heurs = "0" + heurs;
+//if(minites < 10)
+//minites = "0" + minites;
+//if(secondes < 10)
+//secondes = "0" + secondes;
+
+
+//let heurActuelle = heurs + ":" + minites + ":" + secondes;
+
+//document.getElementById("Horloge").textContent = heurActuelle;
+//setInterval("Horloge()", 1000);
+
+
+//}
+
+//document.body.onload = Horloge;
